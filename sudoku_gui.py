5531df9fc11603fccedf6c2746c342008163a414
amidoubleami <aminaarystan@gmail.com>
2023-05-24 16:09:10 +0600

    added main-menu with buttons

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -1,10 +1,14 @@
 from sudoku_alg import valid, solve, find_empty
 from copy import deepcopy
-from sys import exit
+from Button import Button
 import pygame
 import time
 import random
 pygame.init()
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
+
+BG = pygame.image.load("assets/Background.png")
 def generate():
     '''Randomly generates a Sudoku grid/board'''
     while True:  #return will interrupt the loop
@@ -158,14 +162,12 @@ class Tile:
             self.selected = True
         return self.selected
 
-def main():
-    '''Runs the main Sudoku GUI/Game'''
-    screen = pygame.display.set_mode((1000, 590))
-    screen.fill((255, 255, 255))
-    pygame.display.set_caption("Sudoku")
-    icon = pygame.image.load("icon.png")
-    pygame.display.set_icon(icon)
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
 
+def play():
+    screen = pygame.display.set_mode((1020, 590))
+    screen.fill((255, 255, 255))  #color
     #loading screen when generating grid
     font = pygame.font.SysFont('Bahnschrift', 40)
     text = font.render("Generating", True, (0, 0, 0))
@@ -265,11 +267,70 @@ def main():
                             board.tiles[i][j].correct = False
                             board.tiles[i][j].incorrect = False #reset tiles
                     running = False
-
         board.redraw(keyDict, wrong, passedTime)
     while True: #another running loop so that the program ONLY closes when user closes program
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-main()
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "white")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+
+        pygame.display.update()
+
+main_menu()
 pygame.quit()
